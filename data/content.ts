83a6ea4cb68ba7e001f3d6c7a87bf0fad0d61a9b
Amir A <amirrardalan@gmail.com>
2022-08-11 16:00:31 -0700

    Set up initial content

---
+++ data/content.ts
@@ -0,0 +1,17 @@
+// import { themeLight, themeDark } from "@/styles/theme"
+
+export const logo = {
+  title: 'Basketball API',
+  subtitle: 'An NBA UI & Code experiment.',
+  alt: 'Basketball API Logo',
+}
+
+export const homeContent = {
+  meta: {
+    title: 'Basketball API – A Next.js Basketball API experiment by @amirardalan.',
+    description: 'A Next.js Basketball API experiment by @amirardalan.'
+  },
+
+  title: 'Basketball API',
+  description: `A Next.js Basketball API experiment by @amirardalan.`,
+}
---
+++ data/metadata.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  name: 'Basketball',
+  title: `NBA API & UI Demo`,
+  description: `A Next.js Basketball API experiment by @amirardalan`,
+  image: `${process.env.NEXT_PUBLIC_SITE_URL}/thumbnail.jpg`,
+  twitterHandle: '@amirardalan'
+}
